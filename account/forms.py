64f437982f834972cd0be33c6b1fc119ec5d7435
Vladimir <bigcorvet@gmail.com>
2022-12-24 11:45:28 +0200

    add PwdResetForm

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
 
 from .models import MyUser
 
@@ -9,24 +9,26 @@ class RegistrationForm(forms.ModelForm):
         label='Введите имя', max_length=50, help_text='* обязательное поле',
         widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'usernameInput', }))
     email = forms.CharField(label='Ваш email', max_length=150, help_text='* обязательное поле',
-                            error_messages={'обязательно': 'Вы не предоставили email'}, 
+                            error_messages={
+                                'обязательно': 'Вы не предоставили email'},
                             widget=forms.EmailInput(attrs={'class': 'form-control', 'id': 'emailInput'}))
-    password = forms.CharField(label='Введите пароль', min_length=5, 
-                            widget=forms.PasswordInput(attrs={'class':'form-control', 'id': 'passwordlInput'}))
-    password1 = forms.CharField(label='Повторите пароль', min_length=5, 
-                            widget=forms.PasswordInput(attrs={'class':'form-control', 'id': 'passwordConfirmInput'}))
+    password = forms.CharField(label='Введите пароль', min_length=5,
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'passwordlInput'}))
+    password1 = forms.CharField(label='Повторите пароль', min_length=5,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'passwordConfirmInput'}))
 
     class Meta:
         model = MyUser
         fields = ['username', 'email']
-    
+
     def clean_username(self):
         username = self.cleaned_data['username'].lower()
         r = MyUser.objects.filter(username=username)
         if r.count():
-            raise forms.ValidationError('Пользователь с таким именем уже существует')
+            raise forms.ValidationError(
+                'Пользователь с таким именем уже существует')
         return username
-    
+
     def check_password(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
@@ -36,12 +38,26 @@ class RegistrationForm(forms.ModelForm):
     def check_email(self):
         email = self.cleaned_data['email']
         if MyUser.objects.filter(email=email).exists():
-            raise forms.ValidationError('Вы не можете войти на сайт с этим email. Попробуйте ввести другой')
-    
+            raise forms.ValidationError(
+                'Вы не можете войти на сайт с этим email. Попробуйте ввести другой')
+
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(label='Введите email', max_length=150, 
-                    error_messages={'обязательно':'Вы не предоставили email'}, 
-                    widget=forms.TextInput(attrs={'class':'form-control'}))
-    password = forms.CharField(label='Введите пароль', min_length=5, 
-                    widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label='Введите email', max_length=150,
+                               error_messages={
+                                   'обязательно': 'Вы не предоставили email'},
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Введите пароль', min_length=5,
+                               widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class PwdResetForm(PasswordResetForm):
+    email = forms.EmailField(max_length=254, widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'Введите email',}))
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        u = MyUser.objects.filter(email=email)
+        if not u:
+            raise forms.ValidationError('Email не найден')
+        return email
