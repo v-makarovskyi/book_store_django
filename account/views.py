b8e685295ea698c42778ec797adb9601be0c6e4e
Vladimir <bigcorvet@gmail.com>
2022-12-24 08:47:24 +0200

    add views register, activate account and dashboard

--- account/views.py
+++ account/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect, get_object_or_404
+
 from django.contrib import messages
-from django.http import HttpResponseRedirect, HttpResponse
-from .forms import RegistrationForm, UserLoginForm
-from .models import MyUser
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.urls import reverse
+
+from .models import MyUser, Address
+from .forms import RegistrationForm
+from .tokens import account_activation_token
 
 def account_register(request):
-
     if request.user.is_authenticated:
-        return redirect('account:profile')
+        return redirect('account:dashboard')
 
     if request.method == 'POST':
         register_form = RegistrationForm(request.POST)
@@ -17,42 +24,43 @@ def account_register(request):
             user = register_form.save(commit=False)
             user.email = register_form.cleaned_data['email']
             user.set_password(register_form.cleaned_data['password'])
+            user.is_active = False
             user.save()
-
-            user = authenticate(request, username=user.username, password = request.POST['password'])
-            return redirect('bookstore:home')
+            current_site = get_current_site(request)
+            subject = 'Активация учетной записи'
+            message = render_to_string('account/registration/account_activation_email.html',
+                {
+                    'user': user,
+                    'domain': current_site,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user)
+                }
+            )
+            user.email_user(subject=subject, message=message)
+            return render(request, 'account/registration/register_email_confirm.html', {'form': register_form})
         else:
             return HttpResponse('Ошибка обработки данных', status=400)
     else:
         register_form = RegistrationForm()
-    return render(request, 'account/registration/register.html', {'form': register_form})
+        return render(request, 'account/registration/register.html', {'form': register_form})
+    
 
-
-def account_login(request):
-
-    if request.user.is_authenticated:
-        return redirect('account:profile')
-
-    if request.POST.get('submit') == 'login':
-        login_form = UserLoginForm(data=request.POST)
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user:
-            if user.is_active:
-                login(request, user)
-                return redirect('account:profile_user')
-            else:
-                return HttpResponse('Аккаунт не активен')
-        else:
-            return HttpResponse('Что-то пошло не так') 
+def account_activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64)
+        user = MyUser.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, user.DoesNotExists):
+        user = None
+    
+    if user is not None and account_activation_token(user, token):
+        user.is_active = True
+        user.save()
+        return redirect('account:dashboard')
     else:
-        login_form = UserLoginForm()
-    return render(request, 'account/login.html', {'form': login_form})
-
-
-def profile(request):
-    return render(request, 'account/profile/profile.html', {})
+        return render(request, 'account/registration/activation_invalid.html')
 
 
 
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard/dashboard.html', {})
