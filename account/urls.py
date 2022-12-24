4fd51650efc049bcb8973f622ed1424d4d2997f2
Vladimir <bigcorvet@gmail.com>
2022-12-24 08:50:52 +0200

    add urls login, logout, register, dashboard

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,15 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from django.views.generic import TemplateView
-from .views import account_register, account_login, profile
+from django.views.generic.base import TemplateView
 from .forms import UserLoginForm
+from . import views
 
 app_name = 'account'
 
 urlpatterns = [
-    path('register/', account_register, name='register'),
-    path('login/', account_login , name='login'),
-    path('profile/', profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='account/login.html',
+         form_class=UserLoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/account/login/'), name='logout'),
+    path('register/', views.account_register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
