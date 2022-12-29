939529db01d40c01a71658f12823ed1adb16149f
Vladimir <bigcorvet@gmail.com>
2022-12-29 12:54:32 +0200

    add model Category

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,38 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
-from ckeditor_uploader.fields import RichTextUploadingField
+from django.conf import settings
+from django.urls import reverse
 from mptt.models import MPTTModel, TreeForeignKey
 
+class Category(MPTTModel):
+    class Meta:
+        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Рубрики'
+    
+    name = models.CharField('рубрика', max_length=100, help_text='* название основной рубрики книг')
+    slug = models.SlugField(max_length=255)
+    parent = TreeForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True, related_name='children')
+    is_active = models.BooleanField(default=True)
 
-
-class Category(models.Model):
-    title = models.CharField('категория', max_length=100)
-    slug = models.SlugField(max_length=150)
-    parent = TreeForeignKey('self', related_name='children',
-                            on_delete=models.SET_NULL, null=True, blank=True)
-
-    def __str__(self) -> str:
-        return self.title
-
+    def __str__(self):
+        return self.name
+    
     class MPTTMeta:
         order_insertion_by = ['name']
-
-
-class Tag(models.Model):
-    title = models.CharField('тег', max_length=30)
-    slug = models.CharField(max_length=100)
-
-    def __str__(self) -> str:
-        return self.title
-
-
-class Book(models.Model):
-    title = models.CharField('название', max_length=200)
-    author = models.CharField('автор', max_length=150)
-    image = models.ImageField('изображение', upload_to='articles')
-    description = RichTextUploadingField()
-    slug = models.SlugField(max_length=255)
-    price = models.DecimalField('регулярная цена', help_text='max 9999.99', max_digits=7, decimal_places=2, error_messages={
-                                'name': {'max_length': 'Цена должна быть в диапазоне от 1 грн до 9999.99 грн'}})
-    created = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(default=False)
+    
+    
