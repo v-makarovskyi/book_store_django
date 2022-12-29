2b421ac179cc6e71fc7e8503b29aa2164e0964f5
Vladimir <bigcorvet@gmail.com>
2022-12-29 14:11:01 +0200

    add verbose_name

--- bookstore/apps.py
+++ bookstore/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CatalogueConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "bookstore"
+    verbose_name = 'Каталог книг'
