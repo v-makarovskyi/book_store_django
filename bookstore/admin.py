61cc7c72ce6ddf762aea1011ae0cf844cd4f496b
Vladimir <bigcorvet@gmail.com>
2023-01-05 10:10:07 +0200

    register models for bockstore app

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-# Register your models here.
+from .models import (
+    Category,
+    Book,
+    BookComment,
+    BookSpecific,
+    BookSpecificValue,
+    ProductType,
+    Author,
+    Publisher,
+)
+
+admin.site.register(Category, MPTTModelAdmin)
+
+class BookSpecificInline(admin.TabularInline):
+    model = BookSpecific
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [BookSpecificInline]
+
+@admin.register(Publisher)
+class PubliserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+class BookSpecificValueInline(admin.TabularInline):
+    model = BookSpecificValue
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    inlines = [BookSpecificValueInline]
+    list_display = ['id', 'title', 'author', 'category', 'price']
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('-created',)
