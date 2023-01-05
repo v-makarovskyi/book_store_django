a70e392a6937c9defa3050333fe47658ad9a7dde
Vladimir <bigcorvet@gmail.com>
2023-01-05 18:10:36 +0200

    add paths for category books and single

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import HomeView, single_book, order_summary, books
+from .views import HomeView, BookDetailView, order_summary, books
 
 app_name = 'bookstore'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('books/',  books, name='books'), 
-    path('single/', single_book, name='single_book'),
+    path('category/<slug:slug>/',  books, name='category_list'), 
+    path('book/<slug:slug>/<slug:book_slug>/', BookDetailView.as_view(), name='single_book'),
     path('order/', order_summary, name='order'),
 ]
