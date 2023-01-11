53df1110b0a3ece26d1269d6aae90bc6b71d27a8
Vladimir <bigcorvet@gmail.com>
2023-01-11 15:37:06 +0200

    register models delivery & payment methods

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DeliveryOptions, PaymentOptions
+
+admin.site.register(DeliveryOptions)
+admin.site.register(PaymentOptions)
 
 # Register your models here.
